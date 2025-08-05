14f2fb8276fbb2fd8b9cc3e12915f1fc049fa6e6
Aayush Vyas <aayush25vyas@gmail.com>
2025-08-05 23:34:12 +0530

    Creating the single form into multiple form

--- src/components/ButtonComponent.jsx
+++ src/components/ButtonComponent.jsx
@@ -1,6 +1,6 @@
 import styles from "../style/button.module.css"
-export default function ButtonComponent({buttonText}) {
-    return <button className={styles.button}>{buttonText}</button>
+export default function ButtonComponent({buttonText, onClickHandler}) {
+    return <button className={styles.button} onClick={onClickHandler}>{buttonText}</button>
 } 
 
 
--- src/components/FormComponent.jsx
+++ src/components/FormComponent.jsx
@@ -6,9 +6,13 @@ import emailValidator from "../helper/emailValidator";
 import { FormContext } from "../context/FormContext";
 
 export default function FormComponent() {
+  const [step, setStep] = useState(1);
+
   const { formInput } = useContext(FormContext);
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
+  const usernameRef = useRef(null); 
+  const addressRef = useRef(null); 
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
@@ -31,29 +35,78 @@ export default function FormComponent() {
       passwordRef.current.shake();
     }
   };
-
-  return (
-    //! This noValidate prop or attribute will stop the default input field validation in React
-    <form onSubmit={handleFormSubmit} noValidate>
-      <InputComponent
-        inputId={"user-email"}
-        inputName={"user-email"}
-        inputType="email"
-        inputPlaceholder={"Enter the Email here..."}
-        label={"email"}
-        ref={emailRef}
-        checkOnBlur={handleInvalidEmail}
-      />
-      <InputComponent
-        inputId={"user-password"}
-        inputName={"user-password"}
-        inputType="password"
-        inputPlaceholder={"Enter the Password here..."}
-        label={"password"}
-        ref={passwordRef}
-        checkOnBlur={handleInvalidPassword}
-      />
-      <ButtonComponent buttonText={"Submit"} />
-    </form>
-  );
+  if (step === 1) {
+    return (
+      //! This noValidate prop or attribute will stop the default input field validation in React
+      <form onSubmit={handleFormSubmit} noValidate>
+        <InputComponent
+        key={1}
+          inputId={"user-email"}
+          inputName={"user-email"}
+          inputType="email"
+          inputPlaceholder={"Enter the Email here..."}
+          label={"email"}
+          ref={emailRef}
+          checkOnBlur={handleInvalidEmail}
+        />
+        <InputComponent
+        key={2}
+          inputId={"user-password"}
+          inputName={"user-password"}
+          inputType="password"
+          inputPlaceholder={"Enter the Password here..."}
+          label={"password"}
+          ref={passwordRef}
+          checkOnBlur={handleInvalidPassword}
+        />
+        <ButtonComponent
+          buttonText={"Go On Next"}
+          onClickHandler={() => setStep(step + 1)}
+        />
+      </form>
+    );
+  } else if (step === 2) {
+    return (
+      <form onSubmit={handleFormSubmit} noValidate>
+        <InputComponent
+        key={3}
+          inputId={"username-input"}
+          inputName={"username-input"}
+          inputType="text"
+          inputPlaceholder={"Enter the Username here..."}
+          label={"text"}
+          ref={usernameRef}
+          checkOnBlur={() => console.log("Not needed Validation")}
+        />
+        <ButtonComponent
+          buttonText={"Go On Previous"}
+          onClickHandler={() => setStep(step - 1)}
+        />
+        <ButtonComponent
+          buttonText={"Go On Next"}
+          onClickHandler={() => setStep(step + 1)}
+        />
+      </form>
+    );
+  } else if (step === 3) {
+    return (
+      <form onSubmit={handleFormSubmit} noValidate>
+        <InputComponent
+        key={4}
+          inputId={"user-address"}
+          inputName={"user-address"}
+          inputType="text"
+          inputPlaceholder={"Enter the Address here..."}
+          label={"text"}
+          ref={addressRef}
+          checkOnBlur={() => console.log("Not needed Validation")}
+        />
+        <ButtonComponent
+          buttonText={"Go On Previous"}
+          onClickHandler={() => setStep(step - 1)}
+        />
+        <ButtonComponent buttonText={"Submit"} />
+      </form>
+    );
+  }
 }
